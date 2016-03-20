94da482c5f38639d0513090b70427f1d9926a5a7
leonmyhero <leonmyhero@gmail.com>
2016-03-20 16:21:21 +0000

    Update UT.js

--- UT.js
+++ UT.js
@@ -1,3 +1,28 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 var PlayerW = 100;
 var PlayerH = 150;
 var squarePositions = [[498,28],[334,51],[658, 52],[500, 246],[262,216],[728, 213],[162, 381],[344, 444],[621, 434],[827, 385],[481, 557]];
@@ -25,6 +50,13 @@ Player.prototype.draw = function(ctx, optioncolor){
 	//img.src = this.imgsrc;
 	//console.log("x: " + this.x + "y: " + this.y + "w: " + imageW + "h: " + imageH);
 };
+
+Player.prototype.meets = function(x, y) {
+	return (this.x <= x) && (this.x + this.w >= x) && (this.y <= y) && (this.y + this.h >= y) ||
+		   (this.x <= x + this.w) && (this.x + this.w >= x + this.w) && (this.y <= y) && (this.y + this.h >= y) ||
+		   (this.x <= x) && (this.x + this.w >= x) && (this.y <= y + this.h) && (this.y + this.h >= y + this.h) ||
+		   (this.x <= x + this.w) && (this.x + this.w >= x + this.w) && (this.y <= y + this.h) && (this.y + this.h >= y + this.h);
+}
 
 Player.prototype.contains = function(mx, my) {
   "use strict";
@@ -77,7 +109,7 @@ function CanvasState(canvas) {
   canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
   // Up, down, and move are for dragging
   canvas.addEventListener('mousedown', function(e) {
-    var mouse, mx, my, players, l, i, mySel;
+    var mouse, mx, my, players, l, i, mySel, indexofplayer, player;
     mouse = myState.getMouse(e);
     mx = mouse.x;
     my = mouse.y;
@@ -88,6 +120,14 @@ function CanvasState(canvas) {
     for (i = l-1; i >= 0; i -= 1) {
       if (players[i].contains(mx, my)) {
         mySel = players[i];
+		indexofplayer = i;
+		myState.dragoffx = mySel.x - mx;
+		myState.dragoffy = mySel.y - my;
+		if (indexofplayer !== (l-1)) {
+			player = players[l-1];
+			myState.players[l-1] = mySel;
+			myState.players[indexofplayer] = player;
+		}
         // Keep track of where in the object we clicked
         // so we can move it smoothly (see mousemove)
         myState.dragging = true;
@@ -99,7 +139,7 @@ function CanvasState(canvas) {
         return;
       }
     }
-	console.log("player selected");
+	
     // havent returned means we have failed to select anything.
     // If there was an object selected, we deselect it
     //if (myState.selection) {
@@ -117,8 +157,8 @@ function CanvasState(canvas) {
       mouse = myState.getMouse(e);
       // We don't want to drag the object by its top-left corner, we want to drag it
       // from where we clicked. Thats why we saved the offset and use it here
-      myState.selection.x = mouse.x - myState.dragoffx;
-      myState.selection.y = mouse.y - myState.dragoffy;   
+      myState.selection.x = mouse.x + myState.dragoffx;
+      myState.selection.y = mouse.y + myState.dragoffy;   
 	  //console.log("x: " + myState.selection.x + "y: " + myState.selection.y);
       myState.valid = false; // Something's dragging so we must redraw
     } 
@@ -127,7 +167,7 @@ function CanvasState(canvas) {
   }, true);
   
   canvas.addEventListener('mouseup', function(e) {
-	var mx, my, selectx, selecty, selectid, targetx, targety, targetid;
+	var mx, my, selectx, selecty, selectid, targetx, targety, targetid, found;
 	var mouse = myState.getMouse(e),
     mx = mouse.x,
     my = mouse.y;
@@ -140,8 +180,8 @@ function CanvasState(canvas) {
 		//console.log(myState.players.length);
 		for (var i=0; i<myState.players.length; i++) {
 			//console.log (myState.players[i].id);
-			if(myState.players[i].contains(mx, my) && myState.players[i].id !== selectid) {
-				console.log("match");
+			if(myState.players[i].meets(myState.selection.x, myState.selection.y) && myState.players[i].id !== selectid) {
+				found = true;
 				myState.selection.x = myState.players[i].oldx;
 				myState.selection.y = myState.players[i].oldy;
 				myState.selection.oldx = myState.players[i].oldx;
@@ -152,6 +192,10 @@ function CanvasState(canvas) {
 				myState.players[i].oldy = selecty;
 				console.log(selectx + " " + selecty + " " + selectid);
 			}
+		}
+		if (!found) {
+			myState.selection.x = myState.selection.oldx;
+			myState.selection.y = myState.selection.oldy;
 		}
 	}
 	myState.dragging = false;
@@ -180,7 +224,11 @@ function CanvasState(canvas) {
   this.selectionBoxSize = 6;
   this.selectionBoxColor = 'darkred';
   this.interval = 30;
-  setInterval(function() { myState.draw(); }, myState.interval);
+  this.render = function() {
+	requestAnimationFrame(this.render);
+	this.draw();
+  }
+  //setInterval(function() { myState.draw(); }, myState.interval);
 };
 
 CanvasState.prototype.addPlayer = function(Player) {
@@ -204,13 +252,12 @@ CanvasState.prototype.draw = function() {
       this.ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
     }
 	
-	this.valid = true;
+	//this.valid = true;
 
 };
 
 CanvasState.prototype.clear = function() {
   "use strict";
-  console.log("cleared");
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 };
 
@@ -238,23 +285,105 @@ CanvasState.prototype.getMouse = function(e) {
   // We return a simple javascript object (a hash) with x and y defined
   return {x: mx, y: my};
 };
-
+var s = new CanvasState(document.getElementById("canvas1"));
+function render() {
+	s.draw();
+	requestAnimationFrame(render);
+}
 function init() {
 	var x, y;
-	var s = new CanvasState(document.getElementById("canvas1"));
+	var ramID;
+	
 	for (var i=0; i<squarePositions.length; i++) {
 		x = squarePositions[i][0];
 		y = squarePositions[i][1];
-		s.addPlayer(new Player(imagegold, x, y, PlayerW, PlayerH, s, i, x, y));
+		ramID = Math.trunc(Math.random()*10);
+		
+		switch (true) {
+			case (ramID > 0 && ramID < 3):
+				console.log(ramID);
+				s.addPlayer(new Player(imagegold, x, y, PlayerW, PlayerH, s, i, x, y));
+				break;
+			case (ramID >= 3 && ramID <5):
+				console.log(ramID);
+				s.addPlayer(new Player(imageblack, x, y, PlayerW, PlayerH, s, i, x, y));
+				break;
+			case (ramID >= 5 && ramID < 7):
+				console.log(ramID);
+				s.addPlayer(new Player(imageblue, x, y, PlayerW, PlayerH, s, i, x, y));
+				break;
+			case (ramID >=7 && ramID < 10):
+				console.log(ramID);
+				s.addPlayer(new Player(imagehero, x, y, PlayerW, PlayerH, s, i, x, y));
+				break;
+			default:
+				console.log("default: " + ramID);
+				s.addPlayer(new Player(imageempty, x, y, PlayerW, PlayerH, s, i, x, y));
+				break;
+		}
+		render();
+		getimageboard();
+		
+		//s.addPlayer(new Player(imagegold, x, y, PlayerW, PlayerH, s, i, x, y));
 	}
 };
 var imagegold; 
+var imageempty;
+var imagehero;
+var imageblue;
+var imagehero;
 
 function preCachImages() {
+	imageempty = new Image();
+    imageempty.src = "pitch_player.png";
+	imageblue = new Image();
+	imageblue.src = "small-toty.png";
 	imagegold = new Image();
-    imagegold.src = "pitch_player.png";
+	imagegold.src = "small-gold-rare.png";
+	imageblack = new Image();
+	imageblack.src = "small-if-gold.png";
+	imagehero = new Image();
+	imagehero.src = "small-hero.png";
 }
 
+var transparentColor = {
+    r : 255,
+    g : 0,
+    b : 255
+};
+
+var transparentPath = [];
+function getimageboard() {
+	var canvas, ctx;
+	canvas = document.getElementById("canvas2");
+	ctx = canvas.getContext("2d");
+	ctx.drawImage(imageempty,0,0,100,150);
+	var pixels = ctx.getImageData(0,0,100, 150);
+	var p1, p2, p3, p4, p5;
+	//  p4
+	//p2p1p3
+	//  p5
+	
+	for (var i= 0; i<pixels.data.length; i+=4) {
+		p1 = pixels.data[i+3];
+		p2 = pixels.data[i-1] || 0;
+		p3 = pixels.data[i+7] || 0;
+		p4 = pixels.data[i - 401] || 0;
+		p5 = pixels.data[i+403] || 0;
+		
+		if (p2) p2=1;
+		if (p3) p3=1;
+		if (p4) p4=1;
+		if (p5) p5=1;
+		console.log("1:"+p1+"2:"+p2+"3:"+p3+"4:"+p4+"5:"+p5);
+		if ((p1 === 0) && ((p2+p3+p4+p5) === 1)) {
+			transparentPath.push(i);
+		}
+	}
+	for (i=0; i<transparentPath.length; i++) {
+		console.log(transparentPath[i]);
+	}
+}
 window.onload = preCachImages();
 
 init();
