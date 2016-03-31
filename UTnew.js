36052f101537f2c912bade4b83c9eb895bbbed09
leonmyhero <leonmyhero@gmail.com>
2016-03-31 16:15:30 +0100

    Update UTnew.js

--- UTnew.js
+++ UTnew.js
@@ -14,17 +14,27 @@ var squarePositions = [
 	{x:481, y:557}
 ];
 
+
+/* images is an array with image metadata */
+//preload(images);
+
 function SpriteProto() {
 	this.filename = "";
 	this.cardtype = "";
-	this.imgHandle=null;
+	//this.imgHandle=new Image();
+	this.imgHandle = null;
 	this.size = {w:0, h:0};
 	this.load = function(filename, w, h, cardtype) {
 		var targetSpriteProto = this;
 		this.size.w = w;
 		this.size.h = h;
-		var img = new Image();
+		this.cardtype = cardtype;
+		this.filename = filename;
+		img = new Image();
+		var currenttime = new Date().getTime();
+		//var img = new Image();
 		img.onload = function() {
+			console.log("loaded"+" "+ currenttime);
 			targetSpriteProto.imgHandle = img;
 		}
 		img.src = filename;
@@ -45,11 +55,16 @@ function loadProtos() {
 	for(var i=0; i<imgs.length; i++) {
 		var sp = new SpriteProto();
 		sp.load(imgs[i].nm, imgs[i].w, imgs[i].h, imgs[i].ct);
+		//console.log(imgs[i].nm);
 		protoSprites.push(sp);
-		console.log (sp);
+		//console.log (sp);
 	}
 }
-
+//function preload() {
+//	for (var i=0; i<protoSprites.length; i++) {
+//		protoSprites[i].imgHandle.onload
+//	}
+//}
 function SpriteInstance() {
 	this.id =0;
 	this.zIndex = 50;
@@ -59,6 +74,25 @@ function SpriteInstance() {
 }
 
 var spriteInstances = new Array();
+var images = [
+	"pitch_player.png",
+	"small-gold-rare.png",
+	"small-hero.png",
+	"small-if-gold.png",
+	"small-toty.png"
+];
+function preload(imageArray, index) {
+        index = index || 0;
+        if (imageArray && imageArray.length > index) {
+            var img = new Image ();
+            img.onload = function() {
+                preload(imageArray, index + 1);
+            }
+            img.src = images[index];
+	}
+}
+/* images is an array with image metadata */
+
 
 function generateInstances() {
 	//var numSprites = 4096;
@@ -71,25 +105,57 @@ function generateInstances() {
 		var idx = Math.floor(Math.random()*(protoSprites.length));
 		sp.spriteHandle = protoSprites[idx];
 		sp.size = sp.spriteHandle.size;
-		console.log(protoSprites[idx].src);
+		//console.log(protoSprites[idx].imgHandle);
 		spriteInstances.push(sp);
 	}
 }
-
+var can = document.getElementById("canvas1");
+var ctx = can.getContext("2d");
 function drawSprites() {
-	var can = document.getElementById("canvas1");
-	var ctx = can.getContext("2d");
+	//preload(spriteInstances);
 	for(var i=0; i<spriteInstances.length;i++) {
 		var sp = spriteInstances[i];
 		var ps = sp.spriteHandle;
-		console.log (sp.src);
-		ctx.drawImage(ps.imgHandle,sp.pos.x, sp.pos.y);
+		//var myVar = setInterval(function(){
+	//		ps.imgHandle.onload = function() {
+	//			console.log(ps.imgHandle);
+				ctx.drawImage(ps.imgHandle,sp.pos.x, sp.pos.y, cardW, cardH);
+		//	};
+		//},1000);
+		//clearInterval(myVar);
+		//var currenttime = new Date().getTime();
+		//console.log (ps.imgHandle + " " + currenttime);
+		//ps.imgHandle.onload = function() {
+			//console.log(sp.pos.x + " " +sp.pos.y);
+			//ctx.drawImage(ps.imgHandle,sp.pos.x, sp.pos.y, cardW, cardH);
 	}
+		//console.log(ctx);
 }
-window.onload = loadProtos();
+
+function preloads(imageArray, index) {
+        index = index || 0;
+        if (imageArray && imageArray.length > index) {
+            //var img = new Image ();
+			var sp = imageArray[index];
+			var ps = sp.spriteHandle;
+			console.log(ps.imgHandle + index)
+            ps.imgHandle.onload = function() {
+				ctx.drawImage(ps.imgHandle,sp.pos.x, sp.pos.y, cardW, cardH);
+                preload(imageArray, index + 1);
+            }
+		}
+            
+}
+
+window.onload = preload(images);
+
+
 function init() {
 	//loadProtos();
-	generateInstances()
+	//preload();
+	loadProtos();
+	generateInstances();
 	drawSprites();
 }
 init();
+
